c5b4b3e01e24d3d9b88b2d69626663bd419b6546
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-10-01 22:18:10 +0800

    Remove redundant message variable

--- frontend/src/app/components/collab/ChatComponent.tsx
+++ frontend/src/app/components/collab/ChatComponent.tsx
@@ -1,3 +1,4 @@
+import { Message, MessageContent } from "@/components/ai-elements/message";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ChevronRightIcon } from "lucide-react";
@@ -5,10 +6,28 @@ import { ChevronRightIcon } from "lucide-react";
 export default function ChatComponent() {
   const chatMessages = ["HELLO", "HOW ARE YOU?"];
 
+  const currentUser = "ml-auto mr-2";
+  const otherUser = "mr-auto";
+
   return (
     <div className="flex flex-col h-full bg-stone-900 p-1">
-      <div className="flex bg-stone-500 h-full mb-5 rounded-lg"></div>
+      {/* Chat Box */}
+      <div className="flex flex-col bg-stone-500 h-full mb-5 rounded-lg">
+        {chatMessages.map((message, index) => {
+          return (
+            <div key={index} className="flex">
+              <Message
+                from="user"
+                className={index % 2 == 0 ? currentUser : otherUser}
+              >
+                <MessageContent>{message}</MessageContent>
+              </Message>
+            </div>
+          );
+        })}
+      </div>
 
+      {/* Chat Input */}
       <div className="flex w-full mt-auto gap-2">
         <Input className="bg-white" />
         <Button variant="secondary" size="icon" className="size-9">
