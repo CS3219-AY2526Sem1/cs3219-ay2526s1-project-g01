828851d107dd6bbdb47f2f3192ff2f27225f4a1c
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-10-08 12:08:34 +0800

    Add back host

--- user-service/server.js
+++ user-service/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import { connectToDB } from "./model/repository.js";
 
 const port = process.env.SERVICE_PORT || 8080;
+const host = "0.0.0.0"; 
 
 const server = http.createServer(index);
 
@@ -11,7 +12,7 @@ await connectToDB().then(() => {
   console.log("MongoDB Connected!");
 
   server.listen(port, host);
-  console.log("User service server listening on port " + port);
+  console.log("User service server listening on port:" + port);
 }).catch((err) => {
   console.error("Failed to connect to DB");
   console.error(err);
