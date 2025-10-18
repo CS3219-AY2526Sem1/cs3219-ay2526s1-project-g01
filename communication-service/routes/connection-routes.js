ab5aa662211a58cdfc1e5bce55f68466ffd9ed0e
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-10-18 16:34:50 +0800

    Remove unnecessary files

--- communication-service/routes/connection-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ConnectionController = require('../controller/connection-controller');
-
-router.post('/offer', ConnectionController.offerConnection);
-
-router.post('/answer', ConnectionController.answerConnection);
-
-module.exports = router; 
