47f6d876d8b9c4ea24ff0dc7c7dc96c12a64b77e
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-10-21 17:22:53 +0800

    Set up the server for users to exchange PeerIDs

--- communication-service/server.js
+++ communication-service/server.js
@@ -1,17 +1,66 @@
-const express = require('express');
-const cors = require('cors');
-const connectionRoutes = require('./routes/connection-routes');
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
-
+const server = http.createServer(app);
 app.use(cors());
-app.use(express.json());
 
-app.post('/connection', connectionRoutes);
+const io = new Server(server, { cors: { origin: "*" } });
 
-app.listen(PORT, () => {
-  console.log(`Communication service running on port ${PORT}`);
+const sessions = {};
+
+io.on("connection", (socket) => {
+
+  socket.on("join-session", ({ sessionID, username, peerID }) => {
+    socket.join(sessionID);
+    
+    // Create a new session
+    if (!sessions[sessionID]) {
+      sessions[sessionID] = {}
+    }
+
+    // Add user's information to the session
+    sessions[sessionID][username] = { peerID, socketId: socket.id };
+
+    for (const user in sessions[sessionID]) {
+      if (user != username) {
+        io.to(socket.id).emit("peer-ready", {
+          peerID: sessions[sessionID][user].peerID,
+          username: user
+        });
+      }
+    }
+
+    for (const user in sessions[sessionID]) {
+      if (user !== username) {
+        const socketId = sessions[sessionID][user].socketId;
+        io.to(socketId).emit("peer-ready", {
+          peerID: peerID,
+          username: username
+        });
+      }
+    }
+  });
+
+  // Handle disconnections
+  socket.on("disconnect", () => {
+    for (const sessionID in sessions) {
+      for (const username in sessions[sessionID]) {
+        if (sessions[sessionID][username].socketId === socket.id) {
+          delete sessions[sessionID][username];
+          socket.to(sessionID).emit("user-left", { username });
+          if (Object.keys(sessions[sessionID]).length === 0) {
+            delete sessions[sessionID];
+          }
+          break;
+        }
+      }
+    }
+  });
 });
 
-
+server.listen(3001, () => {
+  console.log("Server running on port 3001");
+});
