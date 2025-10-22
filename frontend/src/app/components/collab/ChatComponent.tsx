2d614caf5001bf589b33415c4285169e012f50f1
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-10-22 16:49:38 +0800

    Fix styling

--- frontend/src/app/components/collab/ChatComponent.tsx
+++ frontend/src/app/components/collab/ChatComponent.tsx
@@ -3,18 +3,12 @@ import { Input } from "@/components/ui/input";
 import { SendHorizonalIcon } from "lucide-react";
 
 export default function ChatComponent() {
-
   return (
     <div className="flex flex-col h-full bg-stone-900 p-1">
-      <div className="flex bg-stone-500 h-full mb-3 rounded-lg m-2">
-
-      </div>
+      <div className="flex bg-stone-500 h-full mb-3 rounded-lg m-2"></div>
       <div className="flex w-full mt-auto gap-2 p-2">
         <Input className="bg-white" />
-        <Button 
-          variant="secondary" 
-          size="icon" 
-          className="size-9 rounded-full">
+        <Button variant="secondary" size="icon" className="size-9 rounded-full">
           <SendHorizonalIcon />
         </Button>
       </div>
