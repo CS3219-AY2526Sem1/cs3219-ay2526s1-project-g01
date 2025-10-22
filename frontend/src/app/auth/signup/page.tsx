0ea829c401f432dd546b0838604ba5477ccf0a68
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-10-22 14:32:16 +0800

    Add background image to signup page

--- frontend/src/app/auth/signup/page.tsx
+++ frontend/src/app/auth/signup/page.tsx
@@ -2,7 +2,10 @@ import SignupForm from "@/app/components/auth/SignUpComponent";
 
 export default function SignUpPage() {
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div 
+      className="min-h-screen flex items-center justify-center bg-cover bg-center"
+      style={{ backgroundImage: "url('/login/login-1.jpg')" }}
+    >
       <SignupForm />
     </div>
   );
