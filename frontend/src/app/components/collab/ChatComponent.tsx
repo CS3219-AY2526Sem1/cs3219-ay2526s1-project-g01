4846c166eb8dce2a6f04cfddf544137006a3490a
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-10-24 21:05:45 +0800

    Finalize the webrtc connection between the 2 users in the same session room

--- frontend/src/app/components/collab/ChatComponent.tsx
+++ frontend/src/app/components/collab/ChatComponent.tsx
@@ -55,16 +55,112 @@ export default function ChatComponent() {
         })
       });
 
+
+    // Listen for remote streams sent by other user
+    connectionRef.current.ontrack = (event) => {
+      if (remoteVideoRef.current && event.streams[0]) {
+        remoteVideoRef.current.srcObject = event.streams[0];
+      }
+    }
+
+    // Exchange ice candidates
+    connectionRef.current.onicecandidate = (event) => {
+      if (event.candidate) {
+        socketRef.current?.emit("ice-candidate", {
+          sessionID,
+          username: user?.username,
+          candidate: event.candidate
+        });
+      }
+    }
+
     // Connect to signalling server
     const socket = io("http://localhost:3001");
     socketRef.current = socket;
+
+    // Listen if an offer is made by the other user
+    socket.on("offer-made", async (offer) => {
+      if (connectionRef.current?.signalingState === "stable" && !isCallerRef.current) {
+        await answerCall(offer);
+      }
+    })
+
+    // Listen if an answer is made by the other user
+    socket.on("offer-accepted", async (answer) => {
+      if (connectionRef.current?.signalingState === "have-local-offer") {
+        await connectionRef.current?.setRemoteDescription(answer);
+      }
+    })
+
+    // Exchange ice candidates
+    socket.on("ice-candidate", async (candidate) => {
+      await connectionRef.current?.addIceCandidate(candidate);
+    });
+
+    // Join the session
+    socket.emit("join-session", {
+      sessionID: sessionID,
+      username: user?.username || "anonymous"
+    })
 
     return () => {
       socket.disconnect();
     }
   }, [user]);
 
-  
+  // Once user has joined the session, we check if other user has joined the session as well
+  useEffect(() => {
+
+
+
+    // Check if the other user has joined the session
+    socketRef.current!.on("peer-ready", (username) => {
+
+      const ownUserName = user?.username || "anonymous";
+
+      if (username == ownUserName) return;
+
+      const isCaller = ownUserName < username;
+
+      // If I am the caller, then I will call the other user
+      if (isCaller && currentStreamRef.current) {
+        offerCall();
+      }
+    })
+  }, [user]);
+
+  // Function for creating offer
+  async function offerCall() {
+
+    if (!connectionRef.current) return;
+
+    const offer = await connectionRef.current?.createOffer();
+    await connectionRef.current?.setLocalDescription(offer);
+
+    socketRef.current?.emit("offer", {
+      sessionID,
+      username: user?.username,
+      offer
+    });
+  }
+
+  // Function for acception offer
+  async function answerCall(offer: RTCSessionDescriptionInit) {
+
+    if (!connectionRef.current) return;
+
+    await connectionRef.current?.setRemoteDescription(offer);
+
+    const answer = await connectionRef.current?.createAnswer();
+    await connectionRef.current?.setLocalDescription(answer);
+
+    socketRef.current?.emit("answer", {
+      sessionID,
+      username: user?.username,
+      answer
+    })
+  }
+
   return (
     <div className="flex flex-col h-full bg-stone-900 p-1">
       <div className="flex w-full">
