0db24f464506a0b5a082c928ae016afa7513bea9
CJianzhi <cjianzhi23@gmail.com>
2025-11-03 21:14:31 +0800

    Update endpoint for collab-service

--- frontend/src/services/collabServiceApi.ts
+++ frontend/src/services/collabServiceApi.ts
@@ -1,18 +1,25 @@
 import { useUser } from "@/contexts/UserContext";
 import { getToken } from "./userServiceCookies";
 
+const baseURL = process.env.NEXT_PUBLIC_API_GATEWAY_BASE_URL;
 //Makes delete request to backend to delete userID TO {sessionId, partnerid} mapping
 export async function deleteSession(userId: string) {
   try {
     const token = getToken();
-    const collabURL = process.env.NEXT_PUBLIC_COLLAB_URL;
-    const response = await fetch(`${collabURL}/${encodeURIComponent(userId)}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    const response = await fetch(
+      `${baseURL}/api/collab/${encodeURIComponent(userId)}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      console.log("Failed to delete userId to session mapping");
+    }
   } catch (error) {
     console.error("Error deleting userId to session mapping :", error);
   }
@@ -22,19 +29,25 @@ export async function deleteSession(userId: string) {
 export async function getUserSessionStatus(
   userId: string,
   setSessionId: (sid: string) => void,
-  setshowRejoinRoomDialog: () => void,
+  setshowRejoinRoomDialog: () => void
 ) {
   try {
     console.log("call get endpoint to check if have session");
-    const collabURL = process.env.NEXT_PUBLIC_COLLAB_URL;
     const token = getToken();
-    const response = await fetch(`${collabURL}/${encodeURIComponent(userId)}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    const response = await fetch(
+      `${baseURL}/api/collab/${encodeURIComponent(userId)}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      console.log("Failed to get user session status");
+    }
     const data = await response.json();
     if (data.hasSession) {
       console.log("user has session ongoing");
